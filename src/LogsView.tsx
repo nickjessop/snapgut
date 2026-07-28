f323d908cbbd567764aaae15ac09b7a78086bcc1
Nick Jessop <nick.jessop@icloud.com>
2026-07-28 15:46:37 -0400

    Timeline: use NoteIcon instead of the notes emoji

--- src/LogsView.tsx
+++ src/LogsView.tsx
@@ -14,6 +14,7 @@ import {
   SettingsIcon,
   EditIcon,
   DeleteIcon,
+  NoteIcon,
 } from "./icons";
 import type { Entitlement } from "./session";
 import InstallHint from "./InstallHint";
@@ -316,7 +317,12 @@ function TimelineRow({
               </div>
             </>
           )}
-          {event.note && <div className="sub note">📝 {event.note}</div>}
+          {event.note && (
+            <div className="sub note">
+              <NoteIcon size={13} />
+              {event.note}
+            </div>
+          )}
         </div>
       </div>
     </div>
